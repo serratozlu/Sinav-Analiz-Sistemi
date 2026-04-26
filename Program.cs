b70b1b7ae6348da05d9e66c02161c43b1ff9fd2d
SLILYT <95033985+SLILYT@users.noreply.github.com>
2026-04-26 17:19:01 +0300

    Menü oluşturuldu.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// MENÜ
+
+Console.WriteLine("1 - Tüm Öğrencileri Listele");
+Console.WriteLine("2 - Ortalama Hesapla");
+Console.WriteLine("3 - Geçen/Kalanları Göster");
+Console.WriteLine("4 - En Başarılı Öğrenci");
+
+
+Console.Write("Lütfen seçim yapınız: ");
+int secim = Convert.ToInt32(Console.ReadLine());
